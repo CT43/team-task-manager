58761ba8f27511cf49cfd8e26008c82447c433f3
github username <github email address>
2018-09-10 10:28:42 -0600

    building out application controller for authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,9 +14,9 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:user][:name])
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
-      redirect_to user_path(@user), notice: "Welcome back to the theme park!"
+      redirect_to user_path(@user), notice: "Hi"
     else
-      redirect_to signin_path
+      redirect_to login_path
     end
   end
 
