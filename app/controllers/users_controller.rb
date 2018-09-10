12f601c9028e62f257a24d1c5a0f31baf9a263db
github username <github email address>
2018-09-10 10:27:19 -0600

    added private functions user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,21 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def home
   end
 
   def new
-  end 
+  end
+
+
+  private
+   # Use callbacks to share common setup or constraints between actions.
+   def set_user
+     @user = User.find(params[:id])
+   end
+
+   # Never trust parameters from the scary internet, only allow the white list through.
+   def user_params
+     params.require(:user).permit( :name, :password, :email, :position, :team_id)
+   end
 end
