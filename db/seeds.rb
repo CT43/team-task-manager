f568a67c1f03a34bd8ca84c361bf9731fb4f974a
github username <github email address>
2018-09-10 10:52:55 -0600

    adding team seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Team.create(name: 'Blue')
+Team.create(name: 'Red')
+Team.create(name: 'Green')
+Team.create(name: 'Yellow')
