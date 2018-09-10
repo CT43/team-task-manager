783062df084e0c3f2f8d8b7ea78c96b9d732ca1f
github username <github email address>
2018-09-10 15:07:27 -0600

    create list functionality

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,24 @@
 class ListsController < ApplicationController
+
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(name: params[:name].to_s)
+    @list.user_id = session[:user_id]
+    @list.save
+
+    ##iterate over task[name] to create all instances of tasks
+    @tasks = params[:task][:name]
+    @tasks = @tasks.reject { |c| c.empty? }
+    @tasks.each do |task|
+      @task = Task.new(name: task)
+      @task.list_id = @list.id
+      @task.save
+    end
+    redirect_to list_path
+  end
+  
 end
