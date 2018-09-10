53763c93b77bc586f0cc521301f6fd6975f1c5a2
github username <github email address>
2018-09-10 13:40:11 -0600

    building out amniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,4 +20,22 @@ class SessionsController < ApplicationController
     end
   end
 
+  def createbyfacebook
+    @user = User.find_or_create_by(id: auth['id']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+    end
+
+    session[:user_id] = @user.id
+
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_10_154027) do
+ActiveRecord::Schema.define(version: 2018_09_10_193912) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2018_09_10_154027) do
     t.integer "team_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
+    t.string "image"
   end
 
 end
