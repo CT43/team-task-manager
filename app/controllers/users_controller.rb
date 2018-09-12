cc0ae6e0bb2846b798a546bddbb1866765d2b43d
github username <github email address>
2018-09-12 12:10:48 -0600

    Refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,51 +5,34 @@ class UsersController < ApplicationController
   end
 
   def new
-     @user = User.new
-   end
+    @user = User.new
+  end
 
-   def show
-     @user = current_user
-   end
+  def show
+   @user = current_user
+  end
 
-   def create
-    @user = User.new(user_params)
-     if !params[:new_team_name].empty?
-       @team = Team.create(name: params[:new_team_name])
-       @user.team = @team
-     end
-     respond_to do |format|
-       if @user.save
-         session[:user_id] = @user.id
-         format.html { redirect_to user_path(@user), notice: "Hi" }
-       else
-         format.html { render :new }
-       end
-     end
+  def create
+  @user = User.new(user_params)
+   if !params[:new_team_name].empty?
+     @team = Team.create(name: params[:new_team_name])
+     @user.team = @team
    end
-
-   def update
-     respond_to do |format|
-       if @user.update(user_params)
-         format.html { redirect_to @user, notice: 'User was successfully updated.' }
-       else
-         format.html { render :edit }
-       end
+   respond_to do |format|
+     if @user.save
+       session[:user_id] = @user.id
+       format.html { redirect_to user_path(@user) }
+     else
+       format.html { render :new }
      end
    end
-
-
-   def edit
-   end
-
+  end
 
   private
-   # Use callbacks to share common setup or constraints between actions.
    def set_user
      @user = User.find(params[:id])
    end
 
-   # Never trust parameters from the scary internet, only allow the white list through.
    def user_params
      params.require(:user).permit( :name, :password, :email, :position, :team_id, :new_team_name)
    end
