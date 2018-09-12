1a8204aa2feb5d4362d464fc784b704de75a8756
github username <github email address>
2018-09-12 13:58:22 -0600

    fixed task update bug

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,8 +2,12 @@ class TasksController < ApplicationController
 
   def update
     task = Task.find_by(id: params[:id])
-    task.completed = params[:task][:completed]
-    task.save
+    if current_user.position > task.list.user.position || current_user.list_ids.include?(task.list_id)
+      task.completed = params[:task][:completed]
+      task.save
+    else
+      redirect_to user_lists_path(current_user)
+    end
   end
 
   def tasks_to_complete
